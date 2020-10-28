c7b1eb2d215c471af7dd9eabcae0da2285d0c79d
nyxeet <nyxeet@ukr.net>
2020-10-28 22:02:05 +0200

    123

--- js/task6.js
+++ js/task6.js
@@ -5,5 +5,15 @@ while (input !== null) {
     input = prompt('Введите число');
     total = total + Number(input);
 }
-
 console.log(`Общая сумма чисел равна ${total}`);
+
+// let total = 0;
+// while (true){
+//     let input = prompt('Введите число');
+//     if (input === null) {
+//         break;
+//     }
+//     input = Number(input);
+//     total += input;
+// }
+// console.log(`Общая сумма чисел равна ${total}`);
